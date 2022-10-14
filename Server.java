be04e2b3d81b5aa64e5df6acf311bdaa0a25779d
andremfm <andre_mfm31@hotmail.com>
2022-10-14 19:58:44 +0100

    server e thread

--- Server.java
+++ Server.java
@@ -2,10 +2,31 @@ import java.net.*;
 import java.io.*;
 
 public class Server {
+	static int DEFAULT_PORT=2000;
 	
 	public static void main(String[] args) throws Exception {
+		int port=DEFAULT_PORT;
 		
+		ServerSocket servidor = null;
 		
+		servidor = new ServerSocket(port);
+		
+		System.out.println("Servidor a' espera de ligacoes no porto " + port);
+		
+		while(true) {
+			try {
+
+				Socket ligacao = servidor.accept();
+
+
+				Thread atendedor = new Thread(ligacao);
+				atendedor.start();
+
+			} catch (IOException e) {
+				System.out.println("Erro na execucao do servidor: "+e);
+				System.exit(1);
+			}
+		}
 	}
 	
 }
--- Thread.java
+++ Thread.java
@@ -3,5 +3,28 @@ import java.io.*;
 import java.util.*;
 
 public class Thread extends Thread {
+	Socket ligacao;
+	BufferedReader in;
+	PrintWriter out;
 	
+	public GetPresencesRequestHandler(Socket ligacao) {
+		this.ligacao = ligacao;
+		
+		try {
+			this.in = new BufferedReader (new InputStreamReader(ligacao.getInputStream()));
+			this.out = new PrintWriter(ligacao.getOutputStream());
+		} catch(IOException e) {
+			System.out.println("Erro na execucao do servidor: " + e);
+			System.exit(1);
+		}
+		
+	}
+	
+	public void run() {                
+		try {
+			
+		} catch (IOException e) {
+			System.out.println("Erro na execucao do servidor: " + e);
+			System.exit(1);
+		}
 }
